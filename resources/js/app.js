52f20c85adf0ec26ec500069492b68c079355d3f
ConsuL <consul.developer@gmail.com>
2021-08-06 12:43:12 +0300

    router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,32 @@
 require('./bootstrap');
 
-import Vue from "vue"
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.component('app', require('./components/App').default)
+Vue.use(VueRouter);
+
+import Home from "./components/Home";
+import About from "./components/About";
+import App from "./components/App";
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: About
+        }
+    ]
+})
+
 const app = new Vue ({
     el: '#app',
-
+    components: {App},
+    router,
 })
