c9b523591f687456f6da0ae431a47a0bd52dfd34
ConsuL <consul.developer@gmail.com>
2021-08-06 12:24:04 +0300

    init added vue added app first render

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compotable" content="ie=edge">
+    <title>Trello clone</title>
+    <link rel="stylesheet" href="{{mix('js/app.css')}}">
+</head>
+<body>
+    <div id="app">
+        <app></app>
+    </div>
+<script src="{{mix('/js/app.js')}}"></script>
+</body>
+</html>
