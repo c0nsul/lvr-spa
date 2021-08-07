d432c8d593b1af95b9a7e294b205a6f12c5418d6
ConsuL <consul.developer@gmail.com>
2021-08-07 13:32:28 +0300

    create desk validations

--- app/Http/Requests/DeskStoreRequest.php
+++ app/Http/Requests/DeskStoreRequest.php
@@ -24,7 +24,14 @@ class DeskStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|max:255',
+            'name' => 'required|max:255|unique:desks,name',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.unique' => 'Desk already exist!',
         ];
     }
 }
--- resources/js/components/desks/Desks.vue
+++ resources/js/components/desks/Desks.vue
@@ -1,70 +1,119 @@
 <template>
     <div class="container">
         <h1>Desks</h1>
+
+        <form @submit.prevent="newDesk">
+            <div class="form-group">
+                <input v-model="name" :class="{ 'is-invalid': $v.name.$error}" class="form-control" placeholder="Enter desk name"
+                       type="text">
+                <div v-if="!$v.name.required" class="invalid-feedback">
+                    This field is required!
+                </div>
+                <div v-if="!$v.name.maxLength" class="invalid-feedback">
+                    MAX Characters: {{ $v.name.$params.maxLength.max }}
+                </div>
+            </div>
+            <button class="btn btn-primary" type="submit">Add</button>
+        </form>
+        <div v-if="error" class="alert alert-danger mt-3" role="alert">
+            Erorr data loading! <br>
+            {{errors[0]}}
+        </div>
         <div class="row">
 
-            <div class="col-lg-4" v-for="desk in desks">
-                <div class="card mt-3" >
-                    <router-link class="card-body" :to="{name: 'showDesk', params: {deskId: desk.id}}">
-                        <h4 class="card-title">{{desk.name}}</h4>
+            <div v-for="desk in desks" class="col-lg-4">
+                <div class="card mt-3">
+                    <router-link :to="{name: 'showDesk', params: {deskId: desk.id}}" class="card-body">
+                        <h4 class="card-title">{{ desk.name }}</h4>
                     </router-link>
-                    <button type="button" class="btn btn-danger mt-3" @click="deleteDesk(desk.id)">Remove</button>
+                    <button class="btn btn-danger mt-3" type="button" @click="deleteDesk(desk.id)">Remove</button>
                 </div>
             </div>
         </div>
 
-        <div class="alert alert-danger" role="alert" v-if="error">
-            Erorr data loading!
-        </div>
 
-        <button class="btn btn-primary" type="button" disabled v-if="loading">
-            <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+        <button v-if="loading" class="btn btn-primary" disabled type="button">
+            <span aria-hidden="true" class="spinner-border spinner-border-sm" role="status"></span>
         </button>
-
 
 
     </div>
 </template>
 
 <script>
+import {maxLength, required} from "vuelidate/lib/validators";
+
 export default {
-    data(){
+    data() {
         return {
             desks: [],
             error: false,
+            errors: [],
             loading: true,
+            name: null,
         }
     },
     mounted() {
-       this.getAllDesks();
+        this.getAllDesks();
+    },
+    validations: {
+        name: {
+            required,
+            maxLength: maxLength(255),
+        }
     },
     methods: {
-        getAllDesks(){
+        newDesk() {
+            this.$v.$touch()
+            if (this.$v.$anyError) {
+                return;
+            }
+            axios.post('/api/V1/desks', {
+                name: this.name,
+            })
+                .then(response => {
+                    this.name = '',
+                        this.loading = true,
+                        this.desks = [],
+                        this.getAllDesks();
+                })
+                .finally(() => {
+                    this.loading = false;
+                })
+                .catch(getEerror => {
+                    if (getEerror.response.data.errors.name){
+                        this.errors = [];
+                        this.errors.push(getEerror.response.data.errors.name[0])
+                    }
+                    this.error = true;
+                })
+        },
+        getAllDesks() {
             axios.get('/api/V1/desks')
                 .then(response => {
                     this.desks = response.data.data;
                 })
-                .finally(()=>{
+                .finally(() => {
                     this.loading = false;
                 })
-                .catch(error =>{
+                .catch(error => {
                     console.log(error);
                     this.error = true;
                 })
         },
-        deleteDesk(id){
-            if (confirm('Do you really want to delete?')){
-                axios.post('/api/V1/desks/'+id, {
+        deleteDesk(id) {
+            if (confirm('Do you really want to delete?')) {
+                axios.post('/api/V1/desks/' + id, {
                     _method: 'DELETE',
                 })
                     .then(response => {
                         this.desks = [],
-                        this.getAllDesks();
+                            this.getAllDesks();
                     })
-                    .finally(()=>{
+                    .finally(() => {
                         this.loading = false;
                     })
-                    .catch(error =>{
+                    .catch(error => {
                         console.log(error);
                         this.error = true;
                     })
